595f37b39b563f845bbf2074fe0845306399f50f
luisrojasmercaderia-prog <luisrojas.mercaderia@gmail.com>
2026-06-16 11:26:31 -0400

    Initialize main.jsx with React and HorariosApp

---
+++ src/src/main.jsx
@@ -0,0 +1,9 @@
+import React from "react";
+import ReactDOM from "react-dom/client";
+import HorariosApp from "./HorariosApp.jsx";
+
+ReactDOM.createRoot(document.getElementById("root")).render(
+  <React.StrictMode>
+    <HorariosApp />
+  </React.StrictMode>
+);
