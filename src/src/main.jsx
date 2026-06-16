183358f11992c75716c8b1f5a6779ce0ede968ec
luisrojasmercaderia-prog <luisrojas.mercaderia@gmail.com>
2026-06-16 11:50:21 -0400

    Remove ReactDOM rendering code from main.jsx

--- src/src/main.jsx
+++ src/src/main.jsx
@@ -1,9 +1,1 @@
-import React from "react";
-import ReactDOM from "react-dom/client";
-import HorariosApp from "./HorariosApp.jsx";
 
-ReactDOM.createRoot(document.getElementById("root")).render(
-  <React.StrictMode>
-    <HorariosApp />
-  </React.StrictMode>
-);
